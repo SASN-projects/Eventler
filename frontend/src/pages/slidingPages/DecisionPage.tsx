fa4d10306e358056fe0e7655b1743aa9831ef602
sivan <sivanalc@gmail.com>
2026-05-23 15:47:54 +0300

    update recommendations requests

--- frontend/src/pages/slidingPages/DecisionPage.tsx
+++ frontend/src/pages/slidingPages/DecisionPage.tsx
@@ -5,7 +5,7 @@ import { FullSizeContainer } from "../../components/layouts";
 import BaseQuestions from "./BaseQuestions";
 import RecommendationsPage from "./RecommendationsPage";
 import Slider from "./SliderPage";
-import { fetchSlidesQuestions, submitAnswers } from "./api";
+import { fetchSlidesQuestions, getRecomendationsById, submitAnswers } from "./api";
 import { LOADING_SUBTITLE, LOADING_TITLE } from "./consts";
 import { LoadingContainer, LoadingSubtitle, LoadingTextContainer, LoadingTitle } from "./styles";
 import { type Answers, type DecisionStep, type Question, type Recommendation } from "./types";
@@ -31,25 +31,9 @@ const DecisionPage: FunctionComponent = () => {
         setIsGeneratingRecommendation(true);
         await submitAnswers(eventId, answers);
 
-        // const { data } = await getRecomendationById(eventId);
-        setRecommendations([
-            {
-                "title": "Romantic Evening Stroll and Dinner in Old Jaffa",
-                "description": "Enjoy a charming evening stroll through the ancient alleys and picturesque port of Old Jaffa, taking in the historical ambiance and breathtaking views of the Tel Aviv coastline. Conclude your perfect date with a delightful dinner at a local restaurant offering fresh seafood or authentic Middle Eastern cuisine.",
-                "address": "Old Jaffa Port, Tel Aviv-Yafo, Israel"
-            },
-            {
-                "title": "Lively Dinner at Port Said, Tel Aviv",
-                "description": "Enjoy a vibrant dining experience at Port Said, one of Tel Aviv's most popular and bustling restaurants. Known for its delicious Mediterranean-inspired small plates and lively atmosphere, it's perfect for two people looking for a dynamic evening. The large crowd adds to the energetic vibe, and the menu offers excellent value within your preferred medium budget (50-150 NIS per person). It's easily accessible by car, allowing for a comfortable arrival.",
-                "address": "Har Sinai St 5, Tel Aviv-Yafo"
-            },
-            {
-                "title": "Lively Dinner at Port Said, Tel Aviv",
-                "description": "Enjoy a vibrant dining experience at Port Said, one of Tel Aviv's most popular and bustling restaurants. Known for its delicious Mediterranean-inspired small plates and lively atmosphere, it's perfect for two people looking for a dynamic evening. The large crowd adds to the energetic vibe, and the menu offers excellent value within your preferred medium budget (50-150 NIS per person). It's easily accessible by car, allowing for a comfortable arrival.",
-                "address": "Har Sinai St 5, Tel Aviv-Yafo"
-            }
-        ]);
-    
+        const { data } = await getRecomendationsById(eventId);
+        setRecommendations(data);
+
         setIsGeneratingRecommendation(false);
         setDecisionStep("recommendation");
     };
@@ -87,8 +71,9 @@ const DecisionPage: FunctionComponent = () => {
         ),
         recommendation: (
             <RecommendationsPage
-                recommendations={recommendations}
+                eventId={eventId}
                 onRestart={onRestart}
+                recommendations={recommendations}
             />
         ),
     };
