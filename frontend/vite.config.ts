e06ad85004d22d667dda7af84888a7280411948f
Noam <noamnahman143@gmail.com>
2026-08-22 01:16:24 +0300

    made all the necessery changes for production

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import mkcert from "vite-plugin-mkcert";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), mkcert()],
+  server: {
+    port: 443,
+    host: "0.0.0.0",
+  },
+  preview: {
+    port: 443,
+    host: "0.0.0.0",
+  },
+});
